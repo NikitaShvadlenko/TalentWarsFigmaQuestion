f2616e69335fb984d98c9e42b3bbea33d3df1426
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2024-02-19 19:49:38 +1300

    wip: SearchView

---
+++ TalentWarsFigmaQuestion/Search/SearchView.swift
@@ -0,0 +1,33 @@
+//
+//  SearchView.swift
+//  TalentWarsFigmaQuestion
+//
+//  Created by Nikita Shvad on 19.02.2024.
+//  Copyright © 2024 Nikita Shvadlenko. All rights reserved.
+//
+
+import UIKit
+
+final class SearchView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubviews()
+        makeConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("Not implemented")
+    }
+}
+
+// MARK: - Private
+extension SearchView {
+    private func addSubviews() {
+
+    }
+
+    private func makeConstraints() {
+
+    }
+}
